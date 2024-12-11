c6120eec75da9b029f0968ba0ad1da7d0196a6ac
ajayvaghela362560 <ajayvaghela362560@gmail.com>
2024-12-12 00:05:53 +0530

    demo 555

--- src/components/Demo/Demo.jsx
+++ src/components/Demo/Demo.jsx
@@ -1,46 +1,19 @@
-import React from "react";
 import Banner from "./Banner";
 import Trending from "./Trending";
 import Discover from "./Discover";
-import { Helmet } from "react-helmet";
 
 const Demo = () => {
-
-  const updateMetaTags = () => {
-    const metaTags = [
-      { property: "og:image", content: "https://mint-invoice.com/images/image-376x376.png" },
-      { property: "og:title", content: "Mint-Invoice | Blog" },
-      { property: "og:description", content: "Explore articles and tips on invoice management." },
-      { property: "og:url", content: "https://mint-invoice.com/blog" },
-    ];
-
-    metaTags.forEach(({ property, content }) => {
-      let metaTag = document.querySelector(`meta[property='${property}']`);
-      if (metaTag) {
-        metaTag.setAttribute("content", content);
-      } else {
-        metaTag = document.createElement("meta");
-        metaTag.setAttribute("property", property);
-        metaTag.setAttribute("content", content);
-        document.head.appendChild(metaTag);
-      }
-    });
-  };
-
-  React.useEffect(() => {
-    updateMetaTags();
-  }, []);
-
+  
   return (
     <>
-      <Helmet>
+      <head>
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Mint-Invoice | Blog Title" />
         <meta property="og:description" content="This is blog description."
         />
         <meta property="og:image" content="https://mint-invoice.com/images/image-376x376.png" />
         <meta property="og:url" content="https://mint-invoice.com/" />
-      </Helmet>
+      </head>
 
       <Banner />
       <Trending />
