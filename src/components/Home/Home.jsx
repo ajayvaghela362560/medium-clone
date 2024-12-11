e3bd8c4e84bf369e13231f48e6f84205f0901fbe
ajayvaghela362560 <ajayvaghela362560@gmail.com>
2024-12-11 23:59:31 +0530

    demo 444

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,30 +1,34 @@
 import React from "react";
-import Posts from "../Common/Posts/Posts";
 import Follow from "./UserToFollow/Follow";
-import { Helmet } from "react-helmet";
 
 const Home = () => {
+  const updateMetaTags = () => {
+    const metaTags = [
+      { property: "og:image", content: "https://mint-invoice.com/images/image-376x376.png" },
+      { property: "og:title", content: "Mint-Invoice | Easy Invoicing Software" },
+      { property: "og:description", content: "Simplify your invoicing process with Mint-Invoice. Create professional invoices in minutes with customizable templates and automated payment reminders." },
+      { property: "og:url", content: "https://mint-invoice.com/" },
+    ];
+
+    metaTags.forEach(({ property, content }) => {
+      let metaTag = document.querySelector(`meta[property='${property}']`);
+      if (metaTag) {
+        metaTag.setAttribute("content", content);
+      } else {
+        metaTag = document.createElement("meta");
+        metaTag.setAttribute("property", property);
+        metaTag.setAttribute("content", content);
+        document.head.appendChild(metaTag);
+      }
+    });
+  };
+
+  React.useEffect(() => {
+    updateMetaTags();
+  }, []);
+
   return (
     <>
-      <Helmet>
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Mint-Invoice | Easy Invoicing Software"
-        />
-        <meta
-          property="og:description"
-          content="Simplify your invoicing process with Mint-Invoice. Create professional invoices in minutes with customizable templates and automated payment reminders."
-        />
-        <meta
-          property="og:image"
-          content="https://mint-invoice.com/images/image-376x376.png"
-        />
-        <meta property="og:url" content="https://mint-invoice.com/" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:site_name" content="Mint-Invoice" />
-      </Helmet>
-
       <section className="size flex gap-[5rem] relative">
         <div className="flex-[2] py-10 mb-[4rem]">
           <h1>New Posts</h1>
